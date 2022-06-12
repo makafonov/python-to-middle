898fd244a190e0e4a98282bb0b56c812e693bb17
Denis S <makafonov@gmail.com>
2022-06-12 20:12:22 +0500

    fix reading queues

--- block_1/threads_and_processes/task_1/implementation.py
+++ block_1/threads_and_processes/task_1/implementation.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from dataclasses import dataclass
-from multiprocessing import Manager, Process
+from multiprocessing import Process, Queue
 from threading import Thread
 from typing import Iterable, Optional
 from uuid import UUID, uuid4
@@ -22,68 +22,93 @@ class Task:
     sequence: Iterable[int]
 
 
-def calculate(task: Task, storage) -> None:
-    storage[task.id] = _OPERATIONS[task.command](task.sequence)
+def calculate(task: Task, results_queue: Queue) -> None:
+    results_queue.put([task.id, _OPERATIONS[task.command](task.sequence)])
 
 
-class Front(Process):
-    def __init__(self, tasks_queue, storage) -> None:
-        super().__init__()
+def process_backend(tasks_queue: Queue, results_queue: Queue) -> None:
+    while True:
+        task = tasks_queue.get()
+        if task is None:
+            break
+
+        thread = Thread(
+            target=calculate,
+            args=(
+                task,
+                results_queue,
+            ),
+        )
+        thread.start()
+        thread.join()
+
+
+def process_fronted(tasks_queue, results_queue):
+    return
+
+
+class Worker:
+    def __init__(self, tasks_queue: Queue, results_queue: Queue) -> None:
         self.tasks_queue = tasks_queue
-        self.storage = storage
+        self.results_queue = results_queue
 
-    def call_command(self, command, sequence):
+    def start(self):
+        self.process.start()
+
+    def stop(self):
+        self.tasks_queue.put(None)
+
+
+class Front(Worker):
+    def __init__(self, tasks_queue: Queue, results_queue: Queue) -> None:
+        super().__init__(tasks_queue, results_queue)
+        self.storage = {}
+        self.process = Process(
+            target=process_fronted, args=(self.results_queue, self.storage)
+        )
+
+    def call_command(self, command: str, sequence: Iterable[int]) -> UUID:
         task_id = uuid4()
         self.tasks_queue.put(Task(task_id, command, sequence))
         return task_id
 
     def get_result(self, task_id: UUID) -> Optional[int]:
-        self.tasks_queue.join()
-        return self.storage.pop(task_id)
+        if task_id in self.storage:
+            return self.storage.pop(task_id)
 
-    def run(self):
-        pass
+        id_, result = self.results_queue.get()
+        if id_ == task_id:
+            return result
+
+        self.storage[id_] = result
+        return self.get_result(task_id)
 
 
-class Backend(Process):
-    def __init__(self, tasks_queue, storage) -> None:
-        super().__init__()
-        self.tasks_queue = tasks_queue
-        self.storage = storage
-
-    def run(self) -> None:
-        while not self.tasks_queue.empty():
-            task = self.tasks_queue.get()
-            thread = Thread(
-                target=calculate,
-                args=(
-                    task,
-                    self.storage,
-                ),
-            )
-            thread.start()
-            thread.join()
-            self.tasks_queue.task_done()
+class Backend(Worker):
+    def __init__(self, tasks_queue, results_queue) -> None:
+        super().__init__(tasks_queue, results_queue)
+        self.process = Process(
+            target=process_backend, args=(self.tasks_queue, self.results_queue)
+        )
 
 
 class Composer:
     def __init__(self) -> None:
-        manager = Manager()
-        self.tasks_queue = manager.Queue()
-        self.storage = manager.dict()
-        self.backend = Backend(self.tasks_queue, self.storage)
+        self.tasks_queue = Queue()
+        self.results_queue = Queue()
+        self.backend = Backend(self.tasks_queue, self.results_queue)
+        self.frontend = Front(self.tasks_queue, self.results_queue)
 
     def start(self):
         self.backend.start()
+        self.frontend.start()
 
     def stop(self):
-        self.backend.join()
-        self.front.join()
+        self.backend.stop()
+        self.frontend.stop()
 
     def get_front(self):
-        self.front = Front(self.tasks_queue, self.storage)
-        self.front.start()
-        return self.front
+        return self.frontend
 
 
 if __name__ == '__main__':
