63285b8dc7294e6857507d11f4bf4a3399d85419
Denis S <makafonov@gmail.com>
2022-06-17 23:53:17 +0500

    refactored task of threads and processes

--- block_1/threads_and_processes/task_1/implementation.py
+++ block_1/threads_and_processes/task_1/implementation.py
@@ -1,9 +1,15 @@
+from abc import ABC
+from abc import abstractstaticmethod
 from collections import defaultdict
 from dataclasses import dataclass
-from multiprocessing import Process, Queue
+from multiprocessing import Process
+from multiprocessing import Queue
 from threading import Thread
-from typing import Iterable, Optional
-from uuid import UUID, uuid4
+from typing import Iterable
+from typing import Optional
+from uuid import UUID
+from uuid import uuid4
+
 
 _OPERATIONS = defaultdict(
     lambda: lambda _: None,
@@ -22,93 +28,94 @@ class Task:
     sequence: Iterable[int]
 
 
-def calculate(task: Task, results_queue: Queue) -> None:
-    results_queue.put([task.id, _OPERATIONS[task.command](task.sequence)])
+def calculate(task: Task, results: Queue) -> None:
+    results.put([task.id, _OPERATIONS[task.command](task.sequence)])
 
 
-def process_backend(tasks_queue: Queue, results_queue: Queue) -> None:
-    while True:
-        task = tasks_queue.get()
-        if task is None:
-            break
-
-        thread = Thread(
-            target=calculate,
-            args=(
-                task,
-                results_queue,
-            ),
-        )
-        thread.start()
-        thread.join()
-
-
-def process_fronted(tasks_queue, results_queue):
-    return
-
-
-class Worker:
-    def __init__(self, tasks_queue: Queue, results_queue: Queue) -> None:
-        self.tasks_queue = tasks_queue
-        self.results_queue = results_queue
+class Worker(ABC):
+    def __init__(self, tasks: Queue, results: Queue) -> None:
+        self.worker: Process
+        self.tasks = tasks
+        self.results = results
 
     def start(self):
-        self.process.start()
+        self.worker.start()
 
     def stop(self):
-        self.tasks_queue.put(None)
+        self.worker.terminate()
+        self.worker.join()
+
+    @abstractstaticmethod
+    def _run():
+        pass
 
 
 class Front(Worker):
-    def __init__(self, tasks_queue: Queue, results_queue: Queue) -> None:
-        super().__init__(tasks_queue, results_queue)
+    def __init__(self, tasks: Queue, results: Queue) -> None:
+        super().__init__(tasks, results)
+        self.messages = Queue()
         self.storage = {}
-        self.process = Process(
-            target=process_fronted, args=(self.results_queue, self.storage)
+        self.worker = Process(
+            target=self._run, args=(self.messages, self.tasks)
         )
-
-    def call_command(self, command: str, sequence: Iterable[int]) -> UUID:
-        task_id = uuid4()
-        self.tasks_queue.put(Task(task_id, command, sequence))
-        return task_id
 
     def get_result(self, task_id: UUID) -> Optional[int]:
         if task_id in self.storage:
             return self.storage.pop(task_id)
 
-        id_, result = self.results_queue.get()
+        id_, result = self.results.get()
         if id_ == task_id:
             return result
 
         self.storage[id_] = result
         return self.get_result(task_id)
 
+    def call_command(self, command: str, sequence: Iterable[int]) -> UUID:
+        task_id = uuid4()
+        self.messages.put(Task(task_id, command, sequence))
+        return task_id
 
-class Backend(Worker):
-    def __init__(self, tasks_queue, results_queue) -> None:
-        super().__init__(tasks_queue, results_queue)
-        self.process = Process(
-            target=process_backend, args=(self.tasks_queue, self.results_queue)
+    @staticmethod
+    def _run(messages: Queue, tasks: Queue):
+        while True:
+            message = messages.get()
+            if message:
+                tasks.put(message)
+
+
+class Back(Worker):
+    def __init__(self, tasks: Queue, results: Queue) -> None:
+        super().__init__(tasks, results)
+        self.worker = Process(
+            target=self._run, args=(self.tasks, self.results)
         )
+
+    @staticmethod
+    def _run(tasks: Queue, results: Queue) -> None:
+        while True:
+            task = tasks.get()
+            thread = Thread(target=calculate, args=(task, results))
+            thread.start()
+            thread.join()
 
 
 class Composer:
     def __init__(self) -> None:
-        self.tasks_queue = Queue()
-        self.results_queue = Queue()
-        self.backend = Backend(self.tasks_queue, self.results_queue)
-        self.frontend = Front(self.tasks_queue, self.results_queue)
+        self._tasks = Queue()
+        self._results = Queue()
+        self._front = Front(self._tasks, self._results)
+        self._back = Back(self._tasks, self._results)
 
     def start(self):
-        self.backend.start()
-        self.frontend.start()
+        self._back.start()
+        self._front.start()
 
     def stop(self):
-        self.backend.stop()
-        self.frontend.stop()
+        self._front.stop()
+        self._back.stop()
 
     def get_front(self):
-        return self.frontend
+        return self._front
 
 
 if __name__ == '__main__':
