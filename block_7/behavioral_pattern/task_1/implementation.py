89ec47f7a6f484dbda2073b94849601675734ae2
Denis S <makafonov@gmail.com>
2022-07-21 17:05:20 +0500

    block_7 tasks

--- block_7/behavioral_pattern/task_1/implementation.py
+++ block_7/behavioral_pattern/task_1/implementation.py
@@ -1,40 +1,78 @@
+import operator
+import re
 from abc import abstractmethod
+
+
+_OPERATION_MAP = {
+    '*': operator.mul,
+    '-': operator.sub,
+    '+': operator.add,
+    '/': operator.truediv,
+}
 
 
 class ArithmeticOperation:
 
     def __init__(self, handler=None):
-        """
-            Инициализация класса
+        """Инициализация класса.
+
         :param handler: следующий обработчик в цепочке
         """
         self._next_handler = handler
 
     @abstractmethod
     def calculate(self, operation: str) -> int:
-        """
-            Расчет результата вычислительной операции
-            :param operation: операция записанная в виде строки
-            :return: целочисленный результат
-        """
-        pass
+        """Расчет результата вычислительной операции.
 
+        :param operation: операция записанная в виде строки
+        :return: целочисленный результат
+        """
+        if self._next_handler:
+            return self._next_handler.calculate(operation)
+
+        return None
+
+    def get_operands(self, operation) -> tuple[int, int]:
+        operands = re.findall(r'\d+', operation)
+        if len(operands) != 2:
+            raise ValueError('Неверно заданы операнды')
+
+        return int(operands[0]), int(operands[1])
 
 class MinusOperation(ArithmeticOperation):
-    """Операция вычитания"""
-    # нужно добавить свой код сюда
+    """Операция вычитания."""
 
+    def calculate(self, operation: str):
+        if '-' in operation:
+            return _OPERATION_MAP['-'](*self.get_operands(operation))
+        else:
+            return super().calculate(operation)
 
 class PlusOperation(ArithmeticOperation):
-    """Операция сложения"""
-    # нужно добавить свой код сюда
+    """Операция сложения."""
+
+    def calculate(self, operation: str):
+        if '+' in operation:
+            return _OPERATION_MAP['+'](*self.get_operands(operation))
+        else:
+            return super().calculate(operation)
 
 
 class DevideOperation(ArithmeticOperation):
-    """Операция деления"""
-    # нужно добавить свой код сюда
+    """Операция деления."""
+
+    def calculate(self, operation: str):
+        if '/' in operation:
+            return _OPERATION_MAP['/'](*self.get_operands(operation))
+        else:
+            return super().calculate(operation)
 
 
 class MultiplyOperation(ArithmeticOperation):
-    """Опрация умножения"""
-    # нужно добавить свой код сюда
+    """Опрация умножения."""
+
+    def calculate(self, operation: str):
+        if '*' in operation:
+            return _OPERATION_MAP['*'](*self.get_operands(operation))
+        else:
+            return super().calculate(operation)
